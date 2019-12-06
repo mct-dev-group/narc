35c29f0e9d3500cc5e04f257dac25984b5f20066
mct-Naserdin <469000616@qq.com>
2019-12-06 17:06:32 +0800

    fixed get paln

--- proj/土地整治三维汇报系统/server/app/service/plan.js
+++ proj/土地整治三维汇报系统/server/app/service/plan.js
@@ -23,24 +23,26 @@ class PlansService extends Service {
     }
     // 乡
     if (table_name === 'country') {
-      const country_ids_array_agg = await this.app[DB].query(
-        `select array_agg(gid) from country_village_tree where parent=${gid}`,
+      const village_ids_array_agg = await this.app[DB].query(
+        `select array_agg(gid) from country_village_tree where parent=${gid} and from_table = 'village'`,
         {
           type: sequelize.QueryTypes.SELECT,
         }
       );
-      const country_ids = country_ids_array_agg[0].array_agg;
-      if (!country_ids) return [];
-      const village_gids_array_agg = await this.app[DB].query(
-        `select array_agg(id) from country_village_tree where parent in (${country_ids.toString()}) and from_table = 'village';`,
+      const village_ids = village_ids_array_agg[0].array_agg;
+      if (!village_ids) return [];
+      const sql = `select array_agg(id) from country_village_tree where parent in (${village_ids.toString()}) and from_table = 'plan';`;
+      console.log(sql);
+      const plan_gids_array_agg = await this.app[DB].query(
+        sql,
         {
           type: sequelize.QueryTypes.SELECT,
         }
       );
-      const village_gids = village_gids_array_agg[0].array_agg;
+      const plan_gids = plan_gids_array_agg[0].array_agg;
 
-      return village_gids ? await this.app[DB].query(
-        `select shape_area, status from plan where gid in (${village_gids.toString()})`,
+      return plan_gids ? await this.app[DB].query(
+        `select shape_area, status from plan where gid in (${plan_gids.toString()})`,
         {
           type: sequelize.QueryTypes.SELECT,
         }
@@ -48,15 +50,15 @@ class PlansService extends Service {
     }
     // 村
     if (table_name === 'village') {
-      const village_gids_array_agg = await this.app[DB].query(
-        `select array_agg(id) from country_village_tree where parent = ${gid} and from_table = 'village';`,
+      const plan_gids_array_agg = await this.app[DB].query(
+        `select array_agg(id) from country_village_tree where parent = ${gid} and from_table = 'plan';`,
         {
           type: sequelize.QueryTypes.SELECT,
         }
       );
-      const village_gids = village_gids_array_agg[0].array_agg;
-      return village_gids ? await this.app[DB].query(
-        `select shape_area, status from plan where gid in (${village_gids.toString()})`,
+      const plan_gids = plan_gids_array_agg[0].array_agg;
+      return plan_gids ? await this.app[DB].query(
+        `select shape_area, status from plan where gid in (${plan_gids.toString()})`,
         {
           type: sequelize.QueryTypes.SELECT,
         }
