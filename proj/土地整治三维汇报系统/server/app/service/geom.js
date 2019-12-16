2795d84812cf6806f6d22191259c789f612a96ef
sp5429 <sp5429@outlook.com>
2019-12-16 15:46:52 +0800

    geom.js getAllCountryStatusWeight 增加统计个数

--- proj/土地整治三维汇报系统/server/app/service/geom.js
+++ proj/土地整治三维汇报系统/server/app/service/geom.js
@@ -112,6 +112,7 @@ class GeomService extends Service {
       percentage=percentage===0?0:percentage.toFixed(2)*1;
       resultList.push({
         xzqmc,
+        count: sumMap.size,
         // data: obj,
         percentage,
         geom: turf.center(JSON.parse(geom))
